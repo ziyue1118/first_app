5156ea3e10e83ca7c48c50216559b33ba3cf9b93
ziyue <ziyue@moonlyt.com>
2014-03-25 01:55:44 -0700

    this is customize 2048

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+});
